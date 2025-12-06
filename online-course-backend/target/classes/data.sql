cf45e9d548ac804f1387fc2c46aad32c2914eedb
Aniket Patil <patilaniket2562m@gmail.com>
2025-12-06 17:22:08 +0530

    prepare clean repo for deployment

---
+++ online-course-backend/target/classes/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO users(name, email, password, role)
+VALUES ('Admin User', 'admin@gmail.com', 'admin123', 'ADMIN');
+
+INSERT INTO users(name, email, password, role)
+VALUES ('John Doe', 'john@gmail.com', '123456', 'USER');
+
+INSERT INTO course(title, description)
+VALUES ('Java Full Stack', 'Learn Spring Boot + React'),
+       ('Python with Django', 'Web development using Django');
