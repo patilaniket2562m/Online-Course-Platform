81f60653329ed85d1a4ed5f7cdbe2c9b0a13bcff
Aniket Patil <patilaniket2562m@gmail.com>
2025-12-07 20:54:50 +0530

    4th Commit

--- online-course-backend/src/main/java/com/ocp/onlinecourse/repository/EnrollmentRepository.java
+++ online-course-backend/src/main/java/com/ocp/onlinecourse/repository/EnrollmentRepository.java
@@ -5,6 +5,12 @@ import com.ocp.onlinecourse.model.Enrollment;
 import com.ocp.onlinecourse.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+
     boolean existsByUserAndCourse(User user, Course course);
+
+    // ⭐ REQUIRED FOR EnrollmentService
+    List<Enrollment> findByUserId(Long userId);
 }
